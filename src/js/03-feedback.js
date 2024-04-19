c4d64b894e40808fff39f21f494aff661251ee2b
AlinaPetcu <150749536+AlinaPetcu@users.noreply.github.com>
2024-04-20 00:41:16 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -43,28 +43,6 @@ form.addEventListener("submit", (evt) => {
   form.reset();
 });
 
-const save = (key, value) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error("Set state error: ", error.message);
-  }
-};
-
-const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error("Get state error: ", error.message);
-  }
-};
-
-export default {
-  save,
-  load,
-};
 
 
 
